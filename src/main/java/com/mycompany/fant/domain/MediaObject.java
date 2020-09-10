d0ad52c1d8800062e3d24797c3236ff5abc79a89
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-09-10 16:39:09 +0200

    added mediaobject classes

---
+++ src/main/java/com/mycompany/fant/domain/MediaObject.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fant.domain;
+
+import no.ntnu.tollefsen.auth.User;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ *
+ * @author mikael
+ */
+/**
+ *
+ * @author mikael
+ */
+@Entity
+@Data @EqualsAndHashCode(callSuper = false)
+public class MediaObject extends AbstractDomain {
+    @Id
+    String id;
+    
+    String name;
+    
+    long filesize;
+    String mimeType;
+    
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    User owner;
+
+    protected MediaObject() {
+        super();
+    }
+    
+    public MediaObject(String id, User owner) {
+        this();
+        this.id = id;
+        this.owner = owner;
+    }
+
+    public MediaObject(String id, User owner, String name, long filesize, String mimetype) {
+        this();
+        this.id = id;
+        this.owner = owner;
+        this.name = name;
+        this.filesize = filesize;
+        this.mimeType = mimetype;
+    }
